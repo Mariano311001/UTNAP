ca08fe4890b5293e6e2a696ca658123026cb709f
CristianDelgado31 <cristian-31-10@hotmail.com>
2023-04-17 20:24:42 -0300

    ingreso de map en el form-contact

--- 3 form-contacto/form-contacto.js
+++ 3 form-contacto/form-contacto.js
@@ -15,3 +15,16 @@ jQuery('document').ready(function($) {
 
     })
 })
+
+//API MAP
+function iniciarMap(){
+    var coord = {lat:-34.5887873 ,lng: -58.4154961};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
